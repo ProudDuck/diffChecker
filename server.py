acf493b339a21ffd750eb027d70164e99c9c8bed
ProudDuck <aaron90628@gmail.com>
2015-06-02 16:19:10 +0800

    misc changes

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ class IndexHandler(tornado.web.RequestHandler):
     def get(self):
 	self.render(
 	    "index.html", 
-	    log_objects=svn_operator.extract_log_object()
+	    change_lists=svn_operator.each_change_list(svn_operator.extract_log_object())
 	)
 
 class DiffHandler(tornado.web.RequestHandler):
@@ -34,23 +34,36 @@ class DiffHandler(tornado.web.RequestHandler):
 
 	diff_content = svn_operator.run_svn_diff_command(revisions[0], revisions[1])
 
-	diff_file_name = 'diff' + str(time.time()) + '.log'
-        diff_file = file(diff_file_name, 'w')
-	diff_file.write(diff_content)
-	diff_file.close()
+        diff_html = diff2Html(diff_content)
+        self.write(diff_html)
 
-	diff_html = commands.getoutput('cat ' + diff_file_name + ' | python diff2html.py')
-	html_file_name = 'diff' + str(time.time()) + '.html'
-	html_file = file(html_file_name, 'w')
-	html_file.write(diff_html)
-	html_file.close()
+class FileDiffHandler(tornado.web.RequestHandler):
+    def post(self):
+        file_path = self.get_argument('file_path')
+        revision = self.get_argument('revision')
 
-        self.render(html_file_name)
+        diff_content = svn_operator.diff_specify_file(file_path, revision)
+
+        diff_html = diff2Html(diff_content)
+        self.write(diff_html)
+
+def diff2Html(diff_content):
+    diff_file_name = 'diff' + str(time.time()) + '.log'
+    diff_file = file(diff_file_name, 'w')
+    diff_file.write(diff_content)
+    diff_file.close()
+
+    command = 'python diff2html.py -i %s -t %d -l %d' %(diff_file_name, 4, 160)
+    diff_html = commands.getoutput('python diff2html.py -i ' + diff_file_name + ' -t 4 -l 160')
+
+    os.remove(diff_file_name)
+
+    return diff_html
 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
     app = tornado.web.Application(
-	handlers=[(r"/", IndexHandler), (r'/diff', DiffHandler)],
+	handlers=[(r"/", IndexHandler), (r'/diff', DiffHandler), (r'/fileDiff', FileDiffHandler)],
         static_path=os.path.join(os.path.dirname(__file__), "static")
     )
     http_server = tornado.httpserver.HTTPServer(app)
--- svn_operator.py
+++ svn_operator.py
@@ -3,11 +3,14 @@
 
 import commands
 import sys
+import re
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-class Svn_Log():
+REPO = 'svn://192.168.58.211/B2C/90source/trunk'
+
+class SVN_Log():
     def __init__(self, revision, author, date, comment):
 	self.revision = revision
 	self.author = author
@@ -15,25 +18,33 @@ class Svn_Log():
 	self.comment = comment
    
     def __str__(self):
-	return ' '.join([self.revision, self.author, self.comment, self.date])
+	return ' '.join([self.revision, self.date, self.author, self.comment])
 	
 
+def run_svn_revision_log_command(revision):
+    log_command = 'svn log %s -r %s -v' %(REPO, revision)
+    output = unicode(commands.getoutput(log_command), 'utf-8')
+    
+    change_list = re.compile(r'[AMD] \/.*').findall(output)
+    return change_list
+
 def run_svn_log_command():
-    repo = 'svn://192.168.58.211/B2C/90source/trunk'
     log_limit = 100
 
-    log_command = 'svn log ' + repo + ' --limit ' + str(log_limit)
-
+    log_command = 'svn log %s --limit %d' %(REPO, log_limit)
     return unicode(commands.getoutput(log_command), 'utf-8')
 
 def run_svn_diff_command(r1, r2):
-    repo = 'svn://192.168.58.211/B2C/90source/trunk'
+    log_command = 'svn diff %s -r %s:%s' %(REPO, r1, r2)
 
-    log_command = 'svn diff ' + repo + ' -r ' + r1 + ':' + r2
-
-    #return unicode(commands.getoutput(log_command), 'utf-8')
     return commands.getoutput(log_command)
     
+def diff_specify_file(file_path, revision):
+    repo = 'svn://192.168.58.211/B2C'
+    
+    log_command = 'svn diff -c %s %s' %(revision, repo + file_path[2:])
+    return commands.getoutput(log_command)
+
 def extract_log_object():
     log_content = run_svn_log_command()
     log_objects = []
@@ -47,13 +58,23 @@ def extract_log_object():
 		comment = ss[3].strip()
 		comment = (comment.split('\n')[len(comment.split('\n')) - 1] if len(comment.split('\n')) > 1  else '')
 
-		log_object = Svn_Log(revision, author, date, comment)
+		log_object = SVN_Log(revision, author, date, comment)
 		log_objects.append(log_object)
 
     return log_objects
 
+def each_change_list(log_objects):
+    map = dict()
+    for log_object in log_objects:
+        change_list = run_svn_revision_log_command(log_object.revision)
+        map[log_object] = change_list
+
+    map = sorted(map.iteritems(), key=lambda d:d[0].revision, reverse=True)
+    return map
+
 if __name__ == '__main__':
-    t =  run_svn_diff_command('r7806', 'r7807')
-    f = file('hah.html', 'w')
-    f.write(t)
-    f.close()
+    #t =  run_svn_diff_command('r7806', 'r7807')
+    #run_svn_revision_log_command('r7806')
+    change_lists = each_change_list(extract_log_object())
+    for c in change_lists:
+        print c[1]
